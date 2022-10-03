11f27fe9ee8f726f28f4e4cd2761a086cc7aaa08
Aaron Goin <aarongoin1990@gmail.com>
2022-10-03 14:37:01 -0600

    subtract function and tests

--- operations.test.js
+++ operations.test.js
@@ -2,7 +2,7 @@
 const {sum} = require('./operations')
 
 //TODO: uncomment the next line
-//const {subtract} = require('./operations')
+const {subtract} = require('./operations')
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
@@ -14,4 +14,20 @@ test('adds 0 + 4 to equal 4', () => {
 
 test('adds -1 + (-7) to equal -8', () => {
     expect(sum(-1, -7)).toBe(-8)
+});
+
+test('subtracts 1 - 2 to equal -1', () => {
+    expect(subtract(1, 2)).toBe(-1)
+});
+
+test('subtracts 2 - 1 to equal 1', () => {
+    expect(subtract(2, 1)).toBe(1)
+});
+
+test('subtracts -1 - 2 to equal -3', () => {
+    expect(subtract(-1, 2)).toBe(-3)
+});
+
+test('subtracts -1 - (-2) to equal 1', () => {
+    expect(subtract(-1, -2)).toBe(1)
 });
